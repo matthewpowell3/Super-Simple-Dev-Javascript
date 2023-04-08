d391a178c8ce881a43a76fc111f24497d1793bc2
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-07 22:40:29 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Console Log
-console.log('hello')
+console.log('hello');
 
 // Variable
 
-let name = 'Matthew'
+let name = 'Matthew';
 
 // if statement
 
 const age = 3;
 
-if( age >= 18) {
-  console.log(`You can drive`)
-}else {
-  console.log(`You can't drive`)
+if (18 >= age) {
+  console.log(`You can drive`);
+} else {
+  console.log(`You can't drive`);
 }
